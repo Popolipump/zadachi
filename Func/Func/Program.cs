6f006ca63209bc1ae4e3222700819db149b69ee5
Sergtraxer200 <148421009+Popolipump@users.noreply.github.com>
2025-05-05 16:04:13 +0500

    Add files via upload

---
+++ Func/Func/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[7];
+
+
+            Console.WriteLine("Введите числа в диапазоне (100 - 199)");
+            void calculation(
+            for (int i = 0; i < 7; i++) {
+                Console.WriteLine($"Введите {i + 1} - ое число");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                if (100 > numbers[i] || numbers[i] > 199)
+                {
+                    return;
+                }
+            }
+            Console.Clear();
+            int Sum = 0;
+            for(int i = 0; i < 7;i++) {
+                Sum += numbers[i];
+            }
+
+            Console.WriteLine($"Сумма чисел :{Sum}");
+        }
+    }
+}
---
+++ Proc/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proc
+{
+    internal class Program
+    {
+        //static void MinMax(double x, double y)
+        //{
+        //    if (x > y)
+        //    {
+        //        double z = x;
+        //        x = y;
+        //        y = z;
+        //    }
+
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("a");
+        //    double a = Convert.ToSingle(Console.ReadLine());
+        //    Console.WriteLine("b");
+        //    double b = Convert.ToSingle(Console.ReadLine());
+        //    Console.WriteLine("c");
+        //    double c = Convert.ToSingle(Console.ReadLine());
+        //    Console.WriteLine("d");
+        //    double d = Convert.ToSingle(Console.ReadLine());
+
+        //    MinMax(a, b);
+        //    MinMax(d, c);
+        //    MinMax(a, c);
+        //    MinMax(b, d);
+        //    Console.WriteLine($"Минимальное число {Math.Round(a,2)}");
+        //    Console.WriteLine($"Максимальное число {Math.Round(d, 2)}");
+
+        //static void Even()
+        //{
+        //    //return K % 2 == 0;
+        //    Console.WriteLine("Введите число:");
+        //    int K = Convert.ToInt16(Console.ReadLine());
+        //    bool B;
+        //    if (K%2==0)
+        //    {
+        //        B = true;
+        //    }
+        //    else B = false;
+        //    Console.WriteLine(B);
+        //}
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 0; i <= 10; i++)
+        //    {
+        //        Even();
+        //    }
+        //}
+
+        static void Fib(int N)
+        {
+            Console.WriteLine( N - 1 + N - 2 );
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                int N;
+                Console.WriteLine("Введите число");
+                N = Convert.ToInt32(Console.ReadLine());
+                Fib(N);
+            }
+        }
+    }
+}
+
+
